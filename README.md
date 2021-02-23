4085ae47a6d3309c243a12a1d8583b1ffe7c2485
joaomiiiguel <joaomiguel.farias@gmail.com>
2021-02-23 17:47:36 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## Tecnologias usadas
 
 - [ReactJs](https://pt-br.reactjs.org) - Uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.
+- [Material-UI](https://react-bootstrap.github.io) - React UI component library.
 - [Axios](https://github.com/axios/axios) - É um cliente HTTP que funciona tanto no browser quanto em node.js.
 - [JSON Server](https://www.npmjs.com/package/json-server) - Uma biblioteca capaz de simular uma API.
--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React from 'react'
 import './App.css';
+//import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Box, Card, Typography, CardContent, CardActions, CardHeader, IconButton } from '@material-ui/core';
+import {Delete, Edit} from '@material-ui/icons';
+
+const useStyles = makeStyles({
+  root: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  cardContainer: {
+    textAlign: 'left'
+  },
+  nameTitle: {
+    fontWeight: 'bold',
+  },
+
+});
+
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
+    <Container className={classes.root} maxWidth="sm">
       <h1>People Management Tool</h1>
-    </div>
+      <Box>
+        <Card >
+          <CardHeader
+            title="João Miguel"
+            subheader="Front-end Developer"
+
+            action={
+              <IconButton aria-label="Delete">
+                <Edit />
+                <Delete />
+              </IconButton>
+            }
+          />
+
+          <CardContent>
+            <Typography className={classes.teamName} variant="body2">Email: joaomiguel@mail.com</Typography>
+            <Typography className={classes.teamName} variant="body2">Start Date: 02/2021</Typography>
+          </CardContent>
+          <CardActions>
+
+          </CardActions>
+        </Card>
+      </Box>
+    </Container>
   );
 }
 
